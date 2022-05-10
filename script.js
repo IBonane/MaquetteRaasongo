3e64cc7e536ec85dd5dd492b07ef0c34d2532ba2
ibonane <bonanedjimbala@gmail.com>
2022-05-10 23:51:09 +0200

    Partial - maquette-page-detail-ads

--- script.js
+++ script.js
@@ -48,3 +48,20 @@ $(document).ready(function(){
     });  
         
 });
+
+/*ads of same category in show detail page ads*/
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
